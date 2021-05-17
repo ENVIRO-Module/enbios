5029677aac6f95a80da6432e2c8b8d5b989b040e
Rafael Nebot Medina <rnebot@gmail.com>
2021-05-17 08:48:16 +0100

    Workbook generation function moved to helper.py
    and named "generate_workbook"

--- enbios/processing/nis_file_generator.py
+++ enbios/processing/nis_file_generator.py
@@ -1,11 +1,6 @@
 import pandas as pd
 from typing import List, Tuple
 
-from openpyxl import Workbook
-from openpyxl.utils import get_column_letter
-from openpyxl.utils.dataframe import dataframe_to_rows
-from openpyxl.writer.excel import save_workbook
-
 
 def list_to_dataframe(lst: List) -> pd.DataFrame:
     return pd.DataFrame(data=lst[1:], columns=lst[0])
@@ -160,36 +155,3 @@ def generate_interfaces_command(blocks_as_processor_templates: List) -> pd.DataF
 
     # Convert to pd.DataFrame
     return list_to_dataframe(lst)
-
-
-def generate_workbook_from_list_of_worksheets(output_name: str, lst: List[Tuple[str, pd.DataFrame]]):
-    """
-
-    :param output_name: Name of the output file
-    :param lst: List of tuples (worksheet name, worksheet contents)
-    :return:
-    """
-    # Convert list of pd.DataFrames to Excel workbook
-    wb = Workbook(write_only=True)
-    for name, df in lst:
-        if df.shape[0] < 2:
-            continue
-
-        ws = wb.create_sheet(name)
-        widths = [0]*(df.shape[1]+1)  # A maximum of 100 columns
-        max_columns = 0
-        for r in dataframe_to_rows(df, index=False, header=True):
-            if len(r) > max_columns:
-                max_columns = len(r)
-            for i in range(len(r)):
-                width = int(len(str(r[i])) * 1.1)
-                if width > widths[i]:
-                    widths[i] = width
-
-        for i, column_width in enumerate(widths):
-            ws.column_dimensions[get_column_letter(i+1)].width = column_width
-
-        for r in dataframe_to_rows(df, index=False, header=True):
-            ws.append(r)
-
-    return save_workbook(wb, output_name)
