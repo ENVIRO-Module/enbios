7b239ed033dd40a41daadb959c7bc39666f4456d
Rafael Nebot Medina <rnebot@gmail.com>
2021-12-20 14:09:12 +0000

    Cleaning up "helper.py"

--- enbios/common/helper.py
+++ enbios/common/helper.py
@@ -1,24 +1,16 @@
 import copy
 import json
 import os
-import re
-import tempfile
-import urllib.request
 from io import BytesIO
 from typing import List, Union, BinaryIO
 from zipfile import ZipFile
 
 import numpy as np
 import pandas as pd
-from nexinfosys.command_generators import IType
-from nexinfosys.common.helper import download_file
-from nexinfosys.embedded_nis import NIS
 from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.writer.excel import save_virtual_workbook
-
-from enbios.processing import read_submit_solve_nis_file
 
 JSON_INDENT = 4
 ENSURE_ASCII = False
@@ -124,60 +116,3 @@ def set_zip_timestamp(in_zip: Union[os.PathLike, str, BinaryIO], timestamp=(2020
                 zinfo_new.date_time = timestamp
                 zout.writestr(zinfo_new, data)
     return out_zip
-
-
-def get_valid_name(original_name):
-    prefix = original_name[0] if original_name[0].isalpha() else "_"
-    remainder = original_name[1:] if original_name[0].isalpha() else original_name
-    return prefix + re.sub("[^0-9a-zA-Z_]+", "", remainder)
-
-
-def get_file_url(fn):
-    return "file:" + urllib.request.pathname2url(fn)
-
-
-def prepare_base_state(base_url: str, solve: bool, directory: str = None):
-    from nexinfosys import initialize_configuration
-    initialize_configuration()  # Needed to make download and NIS later work properly
-    tmp_io = download_file(base_url)
-    bytes_io = set_zip_timestamp(tmp_io)
-    hash = hash_array(bytes_io.getvalue())
-    val_name = get_valid_name(base_url)
-    if directory is None:
-        directory = tempfile.gettempdir()
-    hash_file = os.path.join(f"{directory}", f"base.hash.{val_name}")
-    state_file = os.path.join(f"{directory}", f"base.state.{val_name}")
-    if os.path.isfile(hash_file) and os.path.isfile(state_file):
-        with open(hash_file, "rb") as f:
-            cached_hash = f.read()
-        update = cached_hash != hash
-    else:
-        update = True
-
-    if update:
-        temp_name = tempfile.NamedTemporaryFile(dir=directory, delete=False)
-        temp_name = temp_name.name
-        with open(temp_name, "wb") as f:
-            f.write(bytes_io.getvalue())
-        nis, issues = read_submit_solve_nis_file(get_file_url(temp_name), None, solve=solve)
-        os.remove(temp_name)
-        any_error = False
-        for issue in issues:
-            print(issue)
-            if issue.itype == IType.ERROR:
-                any_error = True
-        # Write if there are no errors. IF there is any error generate an Exception
-        if any_error:
-            raise Exception(f'There were errors with the NIS base file {base_url}.')
-        from nexinfosys.serialization import serialize_state
-        serial_state = serialize_state(nis.get_state())
-        nis.close_session()
-        with open(hash_file, "wb") as f:
-            f.write(hash)
-        with open(state_file, "wb") as f:
-            f.write(serial_state)
-    else:
-        with open(state_file, "rb") as f:
-            serial_state = f.read()
-
-    return serial_state
